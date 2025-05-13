fff898be7357d91dbead6bd2148842b93bb11023
Emmanuel Popoola <popoolae1@gmail.com>
2025-05-12 21:53:16 -0500

    Refactor chat and overview and header components for improved structure

--- app/(chat)/page.tsx
+++ app/(chat)/page.tsx
@@ -1,5 +1,5 @@
-import Chat from '../../components/custom/chat';
 import { generateRandomUUID } from '@/lib/utils';
+import Chat from '../../components/custom/chat';
 
 export const revalidate = 0;
 
@@ -7,7 +7,7 @@ export default async function Home() {
   const id = generateRandomUUID();
 
   return (
-    <div className='relative h-[calc(100%-64px)]fg h-dvh pb-4'>
+    <div className='relative h-dvh pb-4'>
       <Chat id={id} initialMessages={[]} />
     </div>
   );
--- components/custom/header.tsx
+++ components/custom/header.tsx
@@ -3,11 +3,20 @@
 import { Plus } from 'lucide-react';
 import { useRouter } from 'next/navigation';
 import { Button } from '../ui/button';
+import { cn } from '@/lib/utils';
+import { Message } from 'ai';
 
-export default function Header() {
+export default function Header({ messages }: { messages: Message[] }) {
   const router = useRouter();
   return (
-    <header className='flex items-center justify-between absolute left-0 right-0 z-10 px-4 md:px-6 h-14 bg-background/90 backdrop-blur border-b border-muted lg:border-none xl:bg-transparent xl:backdrop-blur-none'>
+    <header
+      className={cn(
+        'flex items-center justify-between absolute left-0 right-0 z-10 px-4 md:px-6 h-14 bg-background/90 backdrop-blur border-b border-muted lg:border-none xl:bg-transparent xl:backdrop-blur-none',
+        {
+          'border-none': messages.length === 0,
+        }
+      )}
+    >
       <h1 className='text-foreground/80 text-lg font-bold tracking-wider'>
         AIPRO
       </h1>
