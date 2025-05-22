58c50024a36d0ddb4b3b817f6f7223ca478be6a7
Emmanuel Popoola <popoolae1@gmail.com>
2025-05-22 02:44:38 -0500

    refactor: simplify CodeBlock component and enhance Markdown integration

--- components/custom/code-block.tsx
+++ components/custom/code-block.tsx
@@ -6,39 +6,27 @@ import { oneLight } from 'react-syntax-highlighter/dist/cjs/styles/prism';
 type CodeBoxProps = {
   codeString: string;
   language: string | undefined;
-  isBlockCode: boolean;
 };
 
-function CodeBlock({ language, codeString, isBlockCode }: CodeBoxProps) {
+function CodeBlock({ language, codeString }: CodeBoxProps) {
   return (
-    <>
-      {isBlockCode && (
-        <div className='bg-neutral-50 rounded-lg border border-gray-200 text-sm'>
-          <div className='flex justify-between  items-center h-[40px] bg-neutral-100 py-3 pl-4 rounded-t-lg font-medium font-sans'>
-            <span className='text-gray-600'>
-              {language ? language : 'text'}
-            </span>
-            <Copy text={codeString} className='text-gray-600' />
-          </div>
+    <div className='bg-neutral-50 rounded-lg border border-gray-200 text-sm'>
+      <div className='flex justify-between  items-center h-[40px] bg-neutral-100 py-3 pl-4 rounded-t-lg font-medium font-sans'>
+        <span className='text-gray-600'>{language ? language : 'text'}</span>
+        <Copy text={codeString} className='text-gray-600' />
+      </div>
 
-          <div className='overflow-x-auto not-prose leading-6'>
-            <SyntaxHighlighter
-              PreTag='div'
-              className='!bg-transparent !px-5 !py-5'
-              language={language}
-              style={oneLight}
-            >
-              {codeString}
-            </SyntaxHighlighter>
-          </div>
-        </div>
-      )}
-      {!isBlockCode && (
-        <code className='bg-neutral-200 text-gray-950 rounded py-0.5 px-1.5 text-sm w-fit not-prose'>
+      <div className='overflow-x-auto not-prose leading-6'>
+        <SyntaxHighlighter
+          PreTag='div'
+          className='!bg-transparent !px-5 !py-5'
+          language={language}
+          style={oneLight}
+        >
           {codeString}
-        </code>
-      )}
-    </>
+        </SyntaxHighlighter>
+      </div>
+    </div>
   );
 }
 
--- components/custom/markdown.tsx
+++ components/custom/markdown.tsx
@@ -1,39 +1,44 @@
-import { memo } from 'react';
-import ReactMarkdown, { Components } from 'react-markdown';
-import remarkGfm from 'remark-gfm';
-import rehypeSanitize from 'rehype-sanitize';
-import remarkMath from 'remark-math';
-import rehypeKatex from 'rehype-katex';
-import 'katex/dist/katex.min.css';
-import CodeBlock from './code-block';
+/* eslint-disable @typescript-eslint/no-unused-vars */
+'use client';
+
 import { preprocessLaTeX } from '@/lib/utils';
+import { memo, useRef } from 'react';
+import ReactMarkdown, { Components } from 'react-markdown';
+import rehypeKatex from 'rehype-katex';
+import rehypeSanitize from 'rehype-sanitize';
+import remarkGfm from 'remark-gfm';
+import remarkMath from 'remark-math';
+import CodeBlock from './code-block';
+import 'katex/dist/katex.min.css';
 
 const components: Partial<Components> = {
-  table(props) {
+  table({ node, children, ...props }) {
     return (
       <div className='overflow-x-auto'>
-        <table>{props.children}</table>
+        <table {...props}>{children}</table>
       </div>
     );
   },
   code(props) {
     const { children, className } = props;
     const match = /language-(\w+)/.exec(className || '');
+    const isBlockCode = !!match || !!children?.toString().includes('\n');
+    const code = String(children).replace(/\n$/, '');
+
+    if (isBlockCode) {
+      return <CodeBlock language={match?.[1]} codeString={code} />;
+    }
 
     return (
-      <CodeBlock
-        isBlockCode={!!match || !!children?.toString().includes('\n')}
-        language={match?.[1]}
-        codeString={String(children).replace(/\n$/, '')}
-      />
+      <code className='not-prose bg-neutral-200 rounded py-0.5 px-1 text-sm font-semibold text-foreground/80'>
+        {code}
+      </code>
     );
   },
 
-  pre: ({ children }) => (
-    <pre className='p-0 bg-transparent text-gray-800'>{children}</pre>
-  ),
+  pre: ({ node, children }) => <pre className='not-prose'>{children}</pre>,
 
-  a: ({ children, ...props }) => {
+  a: ({ node, children, ...props }) => {
     return (
       <a
         className='text-blue-600 no-underline hover:underline font-normal'
@@ -45,28 +50,23 @@ const components: Partial<Components> = {
       </a>
     );
   },
-
-  li: ({ children, ...props }) => {
-    return (
-      <li className='marker:text-gray-600' {...props}>
-        {children}
-      </li>
-    );
-  },
 };
 
 function Markdown({ children }: { children: string }) {
   const parsedLatex = preprocessLaTeX(children);
+  const parentRef = useRef<HTMLDivElement>(null);
 
   return (
-    <ReactMarkdown
-      remarkPlugins={[remarkGfm, remarkMath]}
-      rehypePlugins={[rehypeSanitize, rehypeKatex]}
-      components={components}
-      className='prose max-w-none text-gray-900'
-    >
-      {parsedLatex}
-    </ReactMarkdown>
+    <div ref={parentRef}>
+      <ReactMarkdown
+        remarkPlugins={[remarkGfm, remarkMath]}
+        rehypePlugins={[rehypeSanitize, rehypeKatex]}
+        components={components}
+        className='markdown max-w-none text-foreground prose prose-li:marker:text-gray-600'
+      >
+        {parsedLatex}
+      </ReactMarkdown>
+    </div>
   );
 }
 
