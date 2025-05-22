ba200da6667510e41b4d50a525b187992ce825a6
Emmanuel Popoola <popoolae1@gmail.com>
2025-05-22 03:37:27 -0500

    fix: adjust CodeBlock component styles for improved layout and readability

--- components/custom/code-block.tsx
+++ components/custom/code-block.tsx
@@ -10,16 +10,16 @@ type CodeBoxProps = {
 
 function CodeBlock({ language, codeString }: CodeBoxProps) {
   return (
-    <div className='bg-neutral-50 rounded-lg border border-gray-200 text-sm'>
+    <div className='relative bg-neutral-50 rounded-lg border border-gray-200 text-sm'>
       <div className='flex justify-between  items-center h-[40px] bg-neutral-100 py-3 pl-4 rounded-t-lg font-medium font-sans'>
         <span className='text-gray-600'>{language ? language : 'text'}</span>
         <Copy text={codeString} className='text-gray-600' />
       </div>
 
-      <div className='overflow-x-auto not-prose leading-6'>
+      <div className='overflow-x-auto not-prose'>
         <SyntaxHighlighter
           PreTag='div'
-          className='!bg-transparent !px-5 !py-5'
+          className='!bg-transparent !px-5 !py-5 !m-0 !leading-6'
           language={language}
           style={oneLight}
         >
